af8b9b34443ee0cff267ab9d48450ab277748007
Heath Weiss <HeathWeiss@hotmail.com>
2020-04-03 13:40:05 -0600

    cleaned up comments

--- src/Gmsh/ID.hs
+++ src/Gmsh/ID.hs
@@ -1,25 +1,41 @@
 {-# LANGUAGE DeriveDataTypeable #-}
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE NoImplicitPrelude #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE PatternSynonyms #-}
 
-{- | Supply functionality for Ids such as 'Geometry.PointId' and -}
+{- | Supply 'PointId' ADT and associated classes. Is done in a separate module from functionality, to avoid cycle errors.
+
+Imported as part of Gmsh.Gmsh: import qualified Gmsh.Gmsh as Gmsh
+For internal import: import qualified Gmsh.ID as ID
+-}
 module Gmsh.ID(Increment(..), PointId(..)) where
 
 import Import
 import Run
 import RIO.Process
+import RIO
 import qualified Paths_HasMesh
+import qualified RIO.Map as Map
+
+
+
+import qualified Data.Hashable as H
+import qualified Geometry.Geometry as Geo
 
 
 -- | Increment an ID by 1.
 --
---  Used for supplying ID's for 'Geometry.PointId' etc.
+--  Used for supplying 'PointId' for 'Geometry.Vertex.Vertex' etc.
 class Increment a where
-  incr :: a -> a
+  incr :: a -> a -- > Increment the value.
 
 -- | The gmsh Point ID that is associated with a 'Vertex'
-data PointId = PointId {getPointId :: Int}
+data PointId = PointId {_getPointId :: Int}
   deriving (Eq,Show)
 
 instance Increment PointId where
-  incr (PointId id) = PointId $ id + 1
+  incr (PointId id) = PointId $ id + 1 
+
+
+               
