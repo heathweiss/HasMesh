e8c9533424aecef4fcba403a9f1a870f3bb5bb23
Heath Weiss <HeathWeiss@hotmail.com>
2020-04-03 13:42:49 -0600

    added loadTestEnvironment and matched changes to Environment.hs

--- src/Utils/EnvironmentLoader.hs
+++ src/Utils/EnvironmentLoader.hs
@@ -12,11 +12,11 @@ Loads the 'Utils.Environment.Environment' from Environment.yaml. Reads the desig
 it using ''Utils.FileWriter.newDesignName' to ensure it is valid. If it is not valid, an 'Utils.Exceptions.HaMeshException' is thrown.
 This would happen if the Environment.yaml file has in invalid designName.
 -}
-module Utils.EnvironmentLoader(loadEnvironment) where
+module Utils.EnvironmentLoader(loadEnvironment, loadTestEnvironment) where
 
 import RIO
 import qualified RIO.Text as T
-
+import qualified RIO.Map as Map
 
 import qualified Utils.Exceptions as Hex
 import qualified Utils.RunExceptions as HexR
@@ -29,11 +29,25 @@ import qualified Gmsh.Gmsh as Gmsh
 loadEnvironment :: IO (Enviro.Environment)
 loadEnvironment = do
   loaded <- Enviro.loadLoader
-  ioref <- newIORef $ Gmsh.PointId 1 
+  iorefPointIdSupply <- newIORef $ Gmsh.PointId 1
+  iorefPoints <- newIORef $ Map.fromList []
   
   let
-    env_ = Enviro.toEnvironment loaded ioref
+    env_ = Enviro.toEnvironment loaded iorefPointIdSupply iorefPoints
   validDesignName <- HexR.runEitherIO "validDesignName" $ FW.newDesignName $  view FW.designNameL env_
   return $ env_ 
+
+
+-- | Load the global RIO 'Environment', and set values to default values that wil not change. For testing.
+loadTestEnvironment :: IO (Enviro.Environment)
+loadTestEnvironment = do
+  loaded <- Enviro.loadLoader
+  iorefPointIdSupply <- newIORef $ Gmsh.PointId 1
+  iorefPoints <- newIORef $ Map.fromList []
+  
+  let
+    env_ = Enviro.toEnvironment loaded iorefPointIdSupply iorefPoints
+  --validDesignName <- HexR.runEitherIO "validDesignName" $ FW.newDesignName $  view FW.designNameL env_
+  return $ env_ {Enviro.env_designName = "testDesignName"} 
 
 
