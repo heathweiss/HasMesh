5e8240c7aa5dfffede45b9a0b1a76052aa74b62d
Heath Weiss <HeathWeiss@hotmail.com>
2020-04-05 06:29:38 -0600

    initial commit

---
+++ src/Tutorial/T1.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE NoImplicitPrelude #-}
+
+module Tutorial.T1() where
+
+import RIO
+import qualified System.IO as SIO
+import qualified RIO.ByteString as B
+
+import qualified Utils.FileWriter as FW
+import qualified Utils.RunExceptions as HexR
+import qualified Utils.Environment as Enviro
+import qualified Utils.EnvironmentLoader as EnvLdr
+import qualified Geometry.Geometry as Geo
+import qualified Gmsh.IDFx as IDFx
+import qualified Scripting.Scripting as Script
+
+t1 :: IO ()
+t1 = do
+      let
+        createDesign :: (Enviro.HasPointIdSupply env, Enviro.HasPointIdMap env, Enviro.HasGeoFileHandle env, Enviro.HasDesignName env) => RIO env ()
+        createDesign = do
+          --badName <- HexR.runEitherRIO "badName" $ FW.newDesignName ""
+          env <- ask
+          geoFileHandleIORef <- view Enviro.env_geoFileHandleL
+          geoFileHandle <- readIORef geoFileHandleIORef
+          B.hPut geoFileHandle $ Script.writeLC1
+
+          let
+            vertexs = [Geo.newVertex  1 2 3, Geo.newVertex  4 5 6]
+          points <- runRIO env $ IDFx.toPoints vertexs
+          return ()
+      env <- EnvLdr.loadEnvironment
+      designName <-  HexR.runEitherIO "designName" $ FW.newDesignName "t1"
+      --filePath <-  runRIO (env{Enviro.env_designName = designName}) FW.designFilePath
+      
+      handle_ <- SIO.openFile (FW.designFilePath designName) WriteMode
+      handleRef <- newIORef handle_
+      runRIO (env {Enviro.env_geoFileHandle = handleRef}) createDesign
+        `catch`
+        (\(SomeException e) -> do
+            handle' <- readIORef handleRef
+            SIO.hClose handle'
+            isOpen <- SIO.hIsOpen handle'
+            runSimpleApp $ logInfo $ "handle is open: " <> displayShow isOpen
+            runSimpleApp $ logInfo $ displayShow e
+        )
+      handle' <- readIORef handleRef
+      SIO.hClose handle'
---
+++ src/Tutorial/readme.md
@@ -0,0 +1,2 @@
+Matches up with the Onelab Gmsh tutorial filse in gmsh/tutorial.
+So t1.hs will produce the t1.geo file.
