34a8788a7714c4a145dcdf43d7b1518fc164c6f2
Heath Weiss <HeathWeiss@hotmail.com>
2020-04-08 11:54:20 -0600

    added Line tests

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,9 +1,11 @@
 --{-# OPTIONS_GHC -F -pgmF hspec-discover #-}
 import qualified VertexTest as VertexTest
 import qualified FileWriterTest as FileWriterTest
-import ScriptingTest as ScriptingTest
+import qualified ScriptingTest as ScriptingTest
+import qualified LineTest as LineTest
 
 main = do
   VertexTest.runTests
   FileWriterTest.runTests
   ScriptingTest.runTests
+  LineTest.runTests
