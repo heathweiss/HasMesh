241cb5416f83fd0e91d4f90f7a51c9f1fc2a6f51
Heath Weiss <HeathWeiss@hotmail.com>
2020-04-10 09:41:20 -0600

    added SafeList3MinError

--- src/Utils/Exceptions.hs
+++ src/Utils/Exceptions.hs
@@ -7,7 +7,7 @@ Supply custom exceptions.
 
 Supply conversions from Either to exceptions. Might get rid of this in favor of MonadThrow.
 
-import qualified Exceptions as Utils.Hex 
+import qualified Utils.Exceptions as Hex 
 -}
 module Utils.Exceptions(HasMeshException(..)) where
 import RIO
@@ -18,11 +18,12 @@ import qualified Data.IORef as IOref
 -- | Caught by all functions which use the runEither functions, or call a function which in turn uses it.
 data HasMeshException = ZeroLengthName Text -- ^ For any Text or ByteString, for which the length must be > 0.
                      | GeneralException Text -- ^ Catch-all HasMesh Exception.
-  deriving (Typeable, Show)
+                     | SafeList3MinError Text -- ^ 'Gmsh.PointIdList must have length >= 3
+  deriving (Typeable, Show, Eq)
 
 
 instance Exception HasMeshException where
   displayException (GeneralException msg) = show msg
   displayException (ZeroLengthName msg) = "ZeroLengthName" ++ show msg 
-
+  displayException (SafeList3MinError msg) = "SafeList3MinError with length < 3"
 
