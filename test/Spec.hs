26c3789460611aaf803209ceeeca4a41f65c9747
Heath Weiss <HeathWeiss@hotmail.com>
2020-04-11 08:18:45 -0600

    added exceptionsTest

--- test/Spec.hs
+++ test/Spec.hs
@@ -3,9 +3,11 @@ import qualified VertexTest as VertexTest
 import qualified FileWriterTest as FileWriterTest
 import qualified ScriptingTest as ScriptingTest
 import qualified LineTest as LineTest
+import qualified ExceptionsTest as ExceptionsTest
 
 main = do
   VertexTest.runTests
   FileWriterTest.runTests
   ScriptingTest.runTests
   LineTest.runTests
+  ExceptionsTest.runTests
